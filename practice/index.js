7938c35d26cc6719cb43dc75deca2c26172e1998
young <yangxu@yeezon.com>
2018-06-07 23:08:54 +0800

    feat: init

---
+++ practice/index.js
@@ -0,0 +1,10 @@
+
+import Vue from 'vue';
+
+const div = document.createElement('div');
+document.body.appendChild(div);
+
+new Vue({
+    el: '#root',
+    template: '<div>this is content</div>'
+});
